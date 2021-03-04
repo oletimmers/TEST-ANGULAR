6b6e0112a84d62e3e00476beed736b88a83a9833
Ole Timmers <s1115222@studen.hsleiden.nl>
2021-03-04 14:15:44 +0100

    Testspeelplek is zo goed als af

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {FirstComponentComponent} from './first-component/first-component.component';
+import {SecondComponentComponent} from './second-component/second-component.component';
+import {ThirdComponentComponent} from './third-component/third-component.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',  pathMatch: 'full', redirectTo: '/first'},
+  { path: 'first', component: FirstComponentComponent},
+  { path: 'second', component: SecondComponentComponent},
+  { path: 'third', component: ThirdComponentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComponentComponent } from './first-component/first-component.component';
+import { SecondComponentComponent } from './second-component/second-component.component';
+import { ThirdComponentComponent } from './third-component/third-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponentComponent,
+    SecondComponentComponent,
+    ThirdComponentComponent
   ],
   imports: [
     BrowserModule,
