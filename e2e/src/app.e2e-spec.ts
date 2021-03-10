06cf161481b4ad3ba93e12672b25f53fd0535f54
MESteen <martijnvandersteen1999@gmail.com>
2021-03-10 11:02:55 +0100

    2 succes tests - testen dat we Joe terug krijgen van de DB

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import {browser, by, element, logging} from 'protractor';
 
 describe('workspace-project App', () => {
   let page: AppPage;
@@ -8,10 +8,31 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  //SHOULD FAIL
+  // it('should display welcome message', () => {
+  //   page.navigateTo();
+  //   browser.pause();
+  //   expect(page.getTitleText()).toEqual('TEST-ANGULAR app is running!');
+  // });
+
+  it('should route to DB connectie page', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('TEST-ANGULAR app is running!');
+    page.getDBConnectiePageButton().click();
+    expect(page.getDBConnectiePageButton().getText()).toEqual('Interactieve connectie met de db') ;
   });
+
+  it('should show feedback from de db', () => {
+    page.navigateTo();
+    page.getDBConnectiePageButton().click();
+    page.getDBConnectieFeedbackButton().click();
+    expect(page.getDBConnectieText()).toEqual('Joe');
+  });
+
+  // it('should display a persons name', () => {
+  //   page.navigateTo();
+  //   page.getDBConnectiePage().click();
+  //   browser.pause();
+  // });
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -8,4 +8,17 @@ export class AppPage {
   getTitleText() {
     return element(by.css('app-root .content span')).getText() as Promise<string>;
   }
+
+  getDBConnectiePageButton() {
+    return element(by.css('[routerLink="/second"]'));
+  }
+
+  getDBConnectieFeedbackButton() {
+    return element(by.css('app-second-component form button'));
+  }
+
+  getDBConnectieText() {
+    // return element(by.css('app-second-component ')).getText() as Promise<string>;
+    return element(by.id('contentReturned')).getText();
+  }
 }
