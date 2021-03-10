4f9b78edd5922598cf97f8c69999030b17174f6b
Ole Timmers <s1115222@studen.hsleiden.nl>
2021-03-10 10:26:23 +0100

    README uitgebreid Verder nog de darkmode_spec.js nog ietsjes verbeterd.

--- README.md
+++ README.md
@@ -1,7 +1,22 @@
 # TESTANGULAR
 
-## Protractor branch
+## Cypress branch
 <code>npm install cypress</code> om Cypress toe te voegen, mocht dat nog niet het geval zijn.
 
 Om Cypress vervolgens te runnen type:
 <br> <code>npm run cypress:open</code>
+
+De files voor cypress zijn te vinden in: <br> 
+`cypress` <br>
+
+Zelf heb ik al files in `cypress/integration` gezet.
+
+Mochten er nog vragen zijn stel die uiteraard.
+
+Ole
+
+<hr>
+
+#### Handige linkjes verder:
+* https://www.cypress.io/
+* https://docs.cypress.io/guides/getting-started/writing-your-first-test.html#Write-your-first-test
--- cypress/integration/darkmode_spec.js
+++ cypress/integration/darkmode_spec.js
@@ -5,18 +5,18 @@ describe('Darkmodetest', () => {
 
   it('clicking on toggle', () => {
     cy.get('#darkmode-toggle').contains('Darkmode').click();
-  })
+  });
 
   it('clicking on toggle, background should be dark', () => {
     cy.get('#darkmode-toggle').contains('Darkmode').click();
     cy.get('main').should('have.css', 'background-color', 'rgb(43, 43, 43)')
-  })
+  });
 
   it('clicking on toggle, background should be dark, even on reload', () => {
     cy.get('#darkmode-toggle').contains('Darkmode').click();
     cy.reload()
     cy.get('main').should('have.css', 'background-color', 'rgb(43, 43, 43)')
-  })
+  });
 
   it('clicking on toggle, background should be dark, however when cookies are deleted it should be white again', () => {
     cy.get('#darkmode-toggle').contains('Darkmode').click();
@@ -25,5 +25,5 @@ describe('Darkmodetest', () => {
     cy.clearLocalStorage();
     cy.reload();
     cy.get('main').should('have.css', 'background-color', 'rgba(0, 0, 0, 0)')
-  })
+  });
 });
